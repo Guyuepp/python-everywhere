aadeadd9d749b7cf1ac9c455ccdfd47f639b57c6
Guyuepp <2714381581@qq.com>
2026-03-29 23:21:14 +0800

    Revert "style(android): migrate app ui theme to material3"
    This reverts commit 4e5064340f56c9fca15cd6a0f56e51600994d448.

--- android-text-python-app/app/build.gradle.kts
+++ android-text-python-app/app/build.gradle.kts
@@ -50,7 +50,6 @@ chaquopy {
 dependencies {
     implementation("androidx.core:core-ktx:1.16.0")
     implementation("androidx.appcompat:appcompat:1.7.1")
-    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
 
